6d833aec88438f040894f01841170fe051bab822
mtssmursyidulfalah <hafidz.mhmd04@gmail.com>
2026-05-09 17:11:04 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const siswa = [
+
+  {
+    nisn:"1234567890",
+    tanggal:"02-06-2010",
+    nama:"Ahmad Fauzi",
+    kelas:"IX A",
+    status:"LULUS",
+    nilai:"87.25"
+  },
+
+  {
+    nisn:"9876543210",
+    tanggal:"15-01-2010",
+    nama:"Siti Nurhaliza",
+    kelas:"IX B",
+    status:"LULUS",
+    nilai:"89.10"
+  }
+
+];
