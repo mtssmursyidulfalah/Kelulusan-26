827746d6bdee7483e0eff6c4cae433a70be64307
mtssmursyidulfalah <hafidz.mhmd04@gmail.com>
2026-05-09 16:46:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ const targetDate = new Date(
   "2026-06-02T10:00:00+07:00"
 );
 
+const countdown =
+document.getElementById("countdown");
+
+const loginBox =
+document.getElementById("loginBox");
+
+const infoText =
+document.getElementById("infoText");
+
 function updateCountdown(){
 
   const now = new Date();
 
   const diff = targetDate - now;
 
-  const countdown =
-    document.getElementById("countdown");
-
   if(diff <= 0){
 
     countdown.innerHTML =
       "Pengumuman Sudah Dibuka";
+
+    loginBox.style.display = "block";
+
+    infoText.style.display = "none";
 
     return;
   }
@@ -41,3 +51,37 @@ function updateCountdown(){
 setInterval(updateCountdown,1000);
 
 updateCountdown();
+
+let angka1 =
+Math.floor(Math.random()*10);
+
+let angka2 =
+Math.floor(Math.random()*10);
+
+document.getElementById(
+  "captchaText"
+).innerHTML =
+`${angka1} + ${angka2} = ?`;
+
+function cekKelulusan(){
+
+  const captcha =
+  document.getElementById(
+    "captchaInput"
+  ).value;
+
+  if(
+    Number(captcha)
+    !==
+    angka1 + angka2
+  ){
+
+    alert("Captcha Salah");
+
+    return;
+  }
+
+  alert(
+    "Login berhasil (sementara)"
+  );
+}
